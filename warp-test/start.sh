bba440ec101988e9e6edb39a9cbe0b1f1f6de6e8
ToothbrushB <49379586+ToothbrushB@users.noreply.github.com>
2024-06-12 22:19:10 -0500

    cloudflare in progress

---
+++ warp-test/start.sh
@@ -0,0 +1,6 @@
+
+ping 172.16.0.1
+sleep infinity
+# ffmpeg -f video4linux2 -input_format h264 -video_size 1280x720 -framerate 30 -i /dev/video0 -vcodec copy -an test.h264
+# cd /data
+# python -m http.server 80
